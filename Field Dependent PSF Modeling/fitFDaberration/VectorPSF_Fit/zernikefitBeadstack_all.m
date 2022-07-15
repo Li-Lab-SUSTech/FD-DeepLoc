fe9caf5cd59cf9e0d295e31fde923b2cbf6f0797
ltd0924 <32387785+ltd0924@users.noreply.github.com>
2022-07-15 11:36:09 +0800

    Update zernikefitBeadstack_all.m

--- Field Dependent PSF Modeling/fitFDaberration/VectorPSF_Fit/zernikefitBeadstack_all.m
+++ Field Dependent PSF Modeling/fitFDaberration/VectorPSF_Fit/zernikefitBeadstack_all.m
@@ -131,7 +131,7 @@ gauss_psf = gauss_psf/sum(gauss_psf,'all');
 
 tempGauss1=gauss_psf;
 
-if canUseGPU()
+if gpuDeviceCount>0
     [P,model,~] = MLE_FitAbberation_Final_GPU_float(data_double,thetainit_d,paraFitCell,shared_d,0.1,tempGauss1);
 else
     [P,model,~] = MLE_FitAbberation_Final_CPU(data_double,thetainit_d,paraFitCell,shared_d,0.1,tempGauss1);
